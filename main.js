2a4eab4529021fabd560d3776365eb253fb4a26f
Geonwoo <gwkim@pcninc.co.kr>
2021-12-20 10:58:48 +0900

    불필요 삭제 커밋

--- main.js
+++ main.js
@@ -11,14 +11,6 @@ const multer = require('multer');
 const upload = multer({dest: './upload'})
 
 // router 설정
-var dashBoardRouter = require('./dist/routes/dashboard.js');
-var diaryRouter = require('./dist/routes/diary.js');
-var userRouter = require('./dist/routes/user.js');
-var foodRouter = require('./dist/routes/food.js');
-var boardRouter = require('./dist/routes/board.js');
-var goalRouter = require('./dist/routes/goal.js');
-var loveRouter = require('./dist/routes/love.js');
-var rankRouter = require('./dist/routes/rank.js');
 var page1_1Router = require('./dist/routes/page1_1.js');
 var page1_2Router = require('./dist/routes/page1_2.js');
 var page1_3Router = require('./dist/routes/page1_3.js');
@@ -61,14 +53,6 @@ app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static('dist'));
 app.use('/Profile', express.static('./upload'));
 
-app.use('/dashboard', dashBoardRouter);
-app.use('/diary', diaryRouter);
-app.use('/user', userRouter); 
-app.use('/food', foodRouter);
-app.use('/board', boardRouter);
-app.use('/goal', goalRouter);
-app.use('/love', loveRouter);
-app.use('/rank', rankRouter);
 app.use('/page1_1',page1_1Router);
 app.use('/page1_2',page1_2Router);
 app.use('/page1_3',page1_3Router);
@@ -90,8 +74,8 @@ app.use('/page3',page3Router);
 
 module.exports = app;
 
-var port = 5000;
-var hostname = '0.0.0.0';
+var port = 3000;
+var hostname = '192.168.0.137';
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
